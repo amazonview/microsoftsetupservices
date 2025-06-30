38465be6f8d58f8db1f84bde8bb551c1c370ae8e
amazonview <chetijasimran@gmail.com>
2025-06-30 16:26:00 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'microsoft setup'
 
 
 # The full version, including alpha/beta/rc tags
